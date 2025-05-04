0c92e7450f16cf3cdd5f5a24266704618cdf095a
limitatiom <221870230@smail.nju.edu.cn>
2025-05-04 21:14:16 +0800

    feature: LazyAnimate

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LangProvider from "@/features/lang/LangProvider.tsx";
 import AntdProvider from "@/features/antd/AntdProvider.tsx";
 import AntdContextHolder from "@/features/antd/AntdContextHolder.tsx";
+import {LazyAnimate} from "@/features/animate";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <LangProvider>
-        <AntdProvider>
-            <AntdContextHolder>
-                <>
-                    <div>
-                        <a href="https://vite.dev" target="_blank">
-                            <img src={viteLogo} className="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://react.dev" target="_blank">
-                            <img src={reactLogo} className="logo react" alt="React logo" />
-                        </a>
-                    </div>
-                    <h1>Vite + React</h1>
-                    <div className="card">
-                        <button onClick={() => setCount((count) => count + 1)}>
-                            count is {count}
-                        </button>
-                        <p>
-                            Edit <code>src/App.tsx</code> and save to test HMR
-                        </p>
-                    </div>
-                    <p className="read-the-docs">
-                        Click on the Vite and React logos to learn more
-                    </p>
-                </>
+    return (
+        <LangProvider>
+            <AntdProvider>
+                <AntdContextHolder>
+                    <LazyAnimate>
+                        <>
+                            <div>
+                                <a href="https://vite.dev" target="_blank">
+                                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                                </a>
+                                <a href="https://react.dev" target="_blank">
+                                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                                </a>
+                            </div>
+                            <h1>Vite + React</h1>
+                            <div className="card">
+                                <button onClick={() => setCount((count) => count + 1)}>
+                                    count is {count}
+                                </button>
+                                <p>
+                                    Edit <code>src/App.tsx</code> and save to test HMR
+                                </p>
+                            </div>
+                            <p className="read-the-docs">
+                                Click on the Vite and React logos to learn more
+                            </p>
+                        </>
 
-            </AntdContextHolder>
-        </AntdProvider>
-    </LangProvider>
-  )
+                    </LazyAnimate>
+                </AntdContextHolder>
+            </AntdProvider>
+        </LangProvider>
+    )
 }
 
 export default App
