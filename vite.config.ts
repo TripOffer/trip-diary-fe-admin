7a26580b4fbdb6bbbe9313dde7b5fd836c7ad124
limitatiom <221870230@smail.nju.edu.cn>
2025-05-07 11:31:07 +0800

    chore: vite-config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,32 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(configEnv => {
   const viteEnv = loadEnv(configEnv.mode, process.cwd()) as unknown as ImportMetaEnv
-
+  console.log(viteEnv)
   return {
     plugins: [react(), tailwindcss()],
+    base: viteEnv.VITE_APP_BASE_URL,
+    server: {
+      port: viteEnv.VITE_APP_PORT,
+      proxy: {
+        '/api': {
+          target: viteEnv.VITE_SERVICE_PROXY_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: path => path,
+          configure: proxy => {
+            proxy.on('error', err => {
+              console.log('proxy error', err)
+            })
+            proxy.on('proxyReq', (_, req) => {
+              console.log('Sending Request to the Target:', req.method, req.url)
+            })
+            proxy.on('proxyRes', (proxyRes, req) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
+            })
+          },
+        },
+      },
+    },
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
